253780dc51591e96030cffd7a6cc728bcbb3dfdb
Notbook-Nano <marianogirbau@gmail.com>
2022-11-14 18:40:20 -0300

    Dependencia

--- src/Preambulo.cpp
+++ src/Preambulo.cpp
@@ -1,23 +1,17 @@
-/*
- * Preambulo.cpp
- *
- *  Created on: 14 nov. 2022
- *      Author: mlgir
- */
-
 #include "Preambulo.h"
 
-Preambulo::Preambulo(Fecha fecha, int hora, string lugar) {
-	this->Fecha=fecha;
-	this->Hora=hora;
+Preambulo::Preambulo(Fecha fecha, string lugar) {
+	this->FechaP=fecha;
 	this->Lugar=lugar;
 }
 
 Preambulo::~Preambulo() {
-	// TODO Auto-generated destructor stub
 }
 
 Fecha Preambulo::GetFecha(){
-	return this->Fecha;
+	return this->FechaP;
 }
 
+string Preambulo::GetLugar(){
+	return this->Lugar;
+}
